2050dde64dbde84d58b6754bc5309b722afa9b5e
eugemartinez06 <166465369+eugemartinez06@users.noreply.github.com>
2024-08-31 13:14:02 -0300

    cñase8

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+function sumatoria (num1, num2) {
+    let resultado = num1 + num2;
+    console.log (resultado);
+}
+sumatoria (150, 23);
+
+function nombres (nombre) {
+    console.log ("Hola, " + nombre + " cómo estás? ");
+}
+nombres ("Lorenzo");
+
+function mtsCuadrados (largo, ancho) {
+    let mts = largo + ancho;
+    console.log (mts + "m2");
+}
+mtsCuadrados (6, 1);
+mtsCuadrados (3, 5);
